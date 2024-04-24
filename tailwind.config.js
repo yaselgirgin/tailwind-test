475e81b17faf60c15e9b87a273b2f26d139fe157
Yasel Girgin <yaselgirgin@gmail.com>
2024-04-24 08:50:08 +0300

    tailwindcss menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        gemunu: ['Gemunu Libre', 'sans-serif'],
+        open: ['Open Sans', 'sans-serif'],
+      },
+
+      colors: {
+        'nm-blue': '#007BFF',
+        'nm-red': '#DE202A',
+        'nm-orange': '#FD7E14',
+        'nm-aqua': '#0DCAF0',
+        'nm-green': '#28A745',
+        'nm-yellow': '#FFC107',
+        'nm-navy': '#194F90',
+      },
+    },
   },
   plugins: [],
 }
